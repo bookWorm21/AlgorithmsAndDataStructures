332f858dcd190e69d1bb87eb4dd19d6757d6e7dc
bookWorm <golosov2215>
2023-03-22 14:08:51 +0300

    linked list with dummy

---
+++ Solution/Solution/Structures/DoublyLinkedListWithDummy/DoublyLinkedListWithDummy.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AlgorithmsDataStructures.DummyList
+{
+    public abstract class Node
+    {
+        public Node Next;
+        public Node Prev;
+
+        public abstract int Value { get; set; }
+    }
+
+    public class RealNode : Node
+    {
+        public sealed override int Value { get; set; }
+
+        public RealNode(int value)
+        {
+            Value = value;
+        }
+    }
+
+    public class DummyNode : Node
+    {
+        public override int Value
+        {
+            get => throw new Exception("Dummy node haven't value");
+            set => throw new Exception("Dummy node haven't value");
+        }
+    }
+    
+    public class DoublyLinkedListWithDummy
+    {
+        public Node head => dummyNode.Next;
+        public Node tail => dummyNode.Prev;
+
+        private Node dummyNode;
+
+        public DoublyLinkedListWithDummy()
+        {
+            dummyNode = new DummyNode();
+            dummyNode.Next = dummyNode;
+            dummyNode.Prev = dummyNode;
+        }
+        
+        public DoublyLinkedListWithDummy(params int[] values) : this()
+        {
+            foreach (var value in values)
+            {
+                AddInTail(new RealNode(value));
+            }
+        }
+        
+        public static bool operator ==(DoublyLinkedListWithDummy first, DoublyLinkedListWithDummy second)
+        {
+            int firstCount = first.Count();
+            int secondCount = second.Count();
+            
+            if (firstCount != secondCount)
+            {
+                return false;
+            }
+
+            if (firstCount == 0)
+            {
+                return true;
+            }
+
+            if (first.head.Value != second.head.Value || first.tail.Value != second.tail.Value)
+            {
+                return false;
+            }
+            
+            return first.ForwardEnumerable().SequenceEqual(second.ForwardEnumerable())
+                && first.BackEnumerable().SequenceEqual(second.BackEnumerable());
+        }
+
+        public static bool operator !=(DoublyLinkedListWithDummy first, DoublyLinkedListWithDummy second)
+        {
+            return !(first == second);
+        }
+
+        public IEnumerable<int> ForwardEnumerable()
+        {
+            Node current = dummyNode.Next;
+            while (current != dummyNode)
+            {
+                yield return current.Value;
+                current = current.Next;
+            }
+        }
+
+        public IEnumerable<int> BackEnumerable()
+        {
+            Node current = dummyNode.Prev;
+            while (current != dummyNode)
+            {
+                yield return current.Value;
+                current = current.Prev;
+            }
+        }
+
+        public void AddInTail(Node _item)
+        {
+            _item.Next = dummyNode;
+            _item.Prev = dummyNode.Prev;
+            dummyNode.Prev.Next = _item;
+            dummyNode.Prev = _item;
+        }
+
+        public Node Find(int _value)
+        {
+            Node node = head;
+            while (node != dummyNode)
+            {
+                if (node.Value == _value) 
+                    return node;
+                
+                node = node.Next;
+            }
+            return null;
+        }
+
+        public List<Node> FindAll(int _value)
+        {
+            List<Node> nodes = new List<Node>();
+            Node node = head;
+            while (node != dummyNode)
+            {
+                if (node.Value == _value)
+                {
+                    nodes.Add(node);
+                }
+                
+                node = node.Next;
+            }
+            return nodes;
+        }
+
+        public bool Remove(int _value)
+        {
+            Node node = head;
+            while (node != dummyNode)
+            {
+                if (node.Value == _value)
+                {
+                    node.Prev.Next = node.Next;
+                    node.Next.Prev = node.Prev;
+                    return true;
+                }
+                
+                node = node.Next;
+            }
+
+            return false;
+        }
+
+        public void RemoveAll(int _value)
+        {
+            Node node = head;
+            while (node != dummyNode)
+            {
+                if (node.Value == _value)
+                {
+                    node.Prev.Next = node.Next;
+                    node.Next.Prev = node.Prev;
+                }
+
+                node = node.Next;
+            }
+        }
+
+        public void Clear()
+        {
+            dummyNode.Next = dummyNode;
+            dummyNode.Prev = dummyNode;
+        }
+
+        public int Count()
+        {
+            int count = 0;
+            Node node = head;
+            while (node != dummyNode)
+            {
+                ++count;
+                node = node.Next;
+            }
+            
+            return count;
+        }
+
+        public void InsertAfter(Node _nodeAfter, Node _nodeToInsert)
+        {
+            if (_nodeAfter == null)
+            {
+                var temp = dummyNode.Next;
+                dummyNode.Next = _nodeToInsert;
+                _nodeToInsert.Next = temp;
+                _nodeToInsert.Prev = dummyNode;
+                temp.Prev = _nodeToInsert;
+            }
+            else
+            {
+                var temp = _nodeAfter.Next;
+                _nodeAfter.Next = _nodeToInsert;
+                _nodeToInsert.Next = temp;
+                _nodeToInsert.Prev = _nodeAfter;
+                temp.Prev = _nodeToInsert;
+            }
+        }
+    }
+}
---
+++ Solution/Tests/Structures/DoublyLinkedListWithDummyTest.cs
@@ -0,0 +1,271 @@
+﻿using AlgorithmsDataStructures;
+using AlgorithmsDataStructures.DummyList;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Node = AlgorithmsDataStructures.DummyList.Node;
+
+namespace Tests
+{
+    [TestClass]
+    public class DoublyLinkedListWithDummyTest
+    {
+        [TestMethod]
+        public void AddInTail_EmptyList_CorrectHeadAndTail()
+        {
+            DoublyLinkedListWithDummy list = new DoublyLinkedListWithDummy();
+            Node adding = new RealNode(5);
+            list.AddInTail(adding);
+
+            Assert.AreEqual(adding, list.head);
+            Assert.AreEqual(adding, list.tail);
+        }
+
+        [TestMethod]
+        public void AddInTail_ListWithOneElement_CorrectHeadAndTail()
+        {
+            DoublyLinkedListWithDummy list = new DoublyLinkedListWithDummy();
+            Node first = new RealNode(5);
+            Node second = new RealNode(3);
+            list.AddInTail(first);
+            list.AddInTail(second);
+            Assert.AreEqual(first, list.head);
+            Assert.AreEqual(second, list.tail);
+            Assert.AreEqual(second.Prev, first);
+        }
+
+        [TestMethod]
+        public void AddInTail_ListWithTwoElement_CorrectHeadAndTail()
+        {
+            DoublyLinkedListWithDummy list = new DoublyLinkedListWithDummy();
+            Node first = new RealNode(3);
+            Node second = new RealNode(5);
+            Node third = new RealNode(0);
+            list.AddInTail(first);
+            list.AddInTail(second);
+            list.AddInTail(third);
+
+            Assert.AreEqual(first, list.head);
+            Assert.AreEqual(third, list.tail);
+            Assert.AreEqual(first.Next, second);
+            Assert.AreEqual(second.Next, third);
+            Assert.AreEqual(second.Prev, first);
+            Assert.AreEqual(third.Prev, second);
+        }
+        
+        [TestMethod]
+        public void Find_EmptyList_Null()
+        {
+            DoublyLinkedListWithDummy list = new DoublyLinkedListWithDummy();
+            Node actual = list.Find(10);
+
+            Assert.AreEqual(null, actual);
+        }
+
+        [TestMethod]
+        public void Find_ListWithOtherOneElement_Null()
+        {
+            DoublyLinkedListWithDummy list = new DoublyLinkedListWithDummy();
+            list.AddInTail(new RealNode(-3));
+            Node actual = list.Find(10);
+
+            Assert.AreEqual(null, actual);
+        }
+
+        [TestMethod]
+        public void Find_ListWithNeededOneElement_ThisElement()
+        {
+            DoublyLinkedListWithDummy list = new DoublyLinkedListWithDummy();
+            int value = 5;
+            Node expected = new RealNode(value);
+            list.AddInTail(expected);
+            Node actual = list.Find(value);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Find_ListWithElementsWithSameValue_FirstElement()
+        {
+            DoublyLinkedListWithDummy list = new DoublyLinkedListWithDummy();
+            int value = 5;
+            Node expected = new RealNode(value);
+            list.AddInTail(new RealNode(3));
+            list.AddInTail(expected);
+            list.AddInTail(new RealNode(4));
+            list.AddInTail(new RealNode(5));
+            list.AddInTail(new RealNode(7));
+            Node actual = list.Find(value);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FindAll_ListWithoutNeededElements_EmptyResult()
+        {
+            DoublyLinkedListWithDummy list = new DoublyLinkedListWithDummy();
+            int value = 5;
+            list.AddInTail(new RealNode(3));
+            list.AddInTail(new RealNode(4));
+            list.AddInTail(new RealNode(2));
+            list.AddInTail(new RealNode(7));
+
+            int expected = 0;
+            var actual = list.FindAll(value).Count;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FindAll_ListWithSeveralNeededElements_AllElements()
+        {
+            DoublyLinkedListWithDummy list = new DoublyLinkedListWithDummy();
+            int value = 5;
+            list.AddInTail(new RealNode(3));
+            Node first = new RealNode(value);
+            list.AddInTail(first);
+            list.AddInTail(new RealNode(4));
+            Node second = new RealNode(value);
+            list.AddInTail(second);
+            list.AddInTail(new RealNode(7));
+
+            var result = list.FindAll(value);
+
+            if (result.Count < 2)
+            {
+                Assert.Fail();
+                return;
+            }
+
+            Assert.AreEqual(first, result[0]);
+            Assert.AreEqual(second, result[1]);
+        }
+
+        [TestMethod]
+        public void Remove_ListWithoutNeededElement_False()
+        {
+            DoublyLinkedListWithDummy list = new DoublyLinkedListWithDummy(5, 3, 7, 8, 9);
+            bool expected = false;
+            bool actual = list.Remove(10);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Remove_ListWithNeededElement_True()
+        {
+            DoublyLinkedListWithDummy expected = new DoublyLinkedListWithDummy(5, 3, 7, 8, 9);
+            int valueRemove = 3;
+            DoublyLinkedListWithDummy actual = new DoublyLinkedListWithDummy(5, 7, 8, 9);
+            expected.Remove(valueRemove);
+
+            Assert.IsTrue(expected == actual);
+        }
+
+        [TestMethod]
+        public void Remove_ListWithSeveralNeededElement_RemoveFirstElementOnList()
+        {
+            DoublyLinkedListWithDummy expected = new DoublyLinkedListWithDummy(5, 3, 7, 8, 3, 9);
+            int valueRemove = 3;
+            DoublyLinkedListWithDummy actual = new DoublyLinkedListWithDummy(5, 7, 8, 3, 9);
+            expected.Remove(valueRemove);
+
+            Assert.IsTrue(expected == actual);
+        }
+        
+        [TestMethod]
+        public void RemoveAll_ListWithSeverNeededElement_ListWithoutThisElements()
+        {
+            DoublyLinkedListWithDummy expected = new DoublyLinkedListWithDummy(5, 3, 7, 8, 3, 9, 3, 3);
+            int valueRemove = 3;
+            DoublyLinkedListWithDummy actual = new DoublyLinkedListWithDummy(5, 7, 8, 9);
+            expected.RemoveAll(valueRemove);
+
+            Assert.IsTrue(expected == actual);
+        }
+
+        [TestMethod]
+        public void Count_EmptyList_0()
+        {
+            DoublyLinkedListWithDummy list = new DoublyLinkedListWithDummy();
+            var expected = 0;
+            var actual = list.Count();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Count_ListWithOneElement_1()
+        {
+            DoublyLinkedListWithDummy list = new DoublyLinkedListWithDummy(1);
+            var expected = 1;
+            var actual = list.Count();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Count_ListWithSeveralElements_CorrectValue()
+        {
+            DoublyLinkedListWithDummy list = new DoublyLinkedListWithDummy(5, 3, 7, 8, 9);
+            var expected = 5;
+            var actual = list.Count();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Clear_ListWithSeveralElements_EmptyList()
+        {
+            DoublyLinkedListWithDummy list = new DoublyLinkedListWithDummy(5, 3, 8, 7, 9);
+            var expected = 0;
+            list.Clear();
+            var actual = list.Count();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void InsertAfter_InsertAfterNull_HeadIsInsertedNode()
+        {
+            DoublyLinkedListWithDummy list = new DoublyLinkedListWithDummy(5, 3, 7, 8, 9, 10);
+            Node expected = new RealNode(5);
+            list.InsertAfter(null, expected);
+
+            Assert.AreEqual(expected, list.head);
+        }
+
+        [TestMethod]
+        public void InsertAfter_InsertAfterTail_NewTailIsInsertedNode()
+        {
+            DoublyLinkedListWithDummy list = new DoublyLinkedListWithDummy(5, 3, 7, 8, 9, 10);
+            Node expected = new RealNode(5);
+            Node expectedPrev = list.tail;
+            list.InsertAfter(list.tail, expected);
+
+            Assert.AreEqual(expectedPrev, list.tail.Prev);
+            Assert.AreEqual(expected, list.tail);
+        }
+
+        [TestMethod]
+        public void InsertAfter_InsertInEmptyList_NewTailAndHeadIsInsertedNode()
+        {
+            DoublyLinkedListWithDummy list = new DoublyLinkedListWithDummy();
+            Node expected = new RealNode(5);
+            list.InsertAfter(null, expected);
+
+            Assert.AreEqual(expected, list.head);
+            Assert.AreEqual(expected, list.tail);
+        }
+
+        [TestMethod]
+        public void InsertAfter_InsertOnMiddleList_CorrectList()
+        {
+            DoublyLinkedListWithDummy actual = new DoublyLinkedListWithDummy(5, 3, 7, 8, 9, 10);
+            Node adding = new RealNode(5);
+            actual.InsertAfter(actual.Find(7), adding);
+            
+            DoublyLinkedListWithDummy expected = new DoublyLinkedListWithDummy(5, 3, 7, 5, 8, 9, 10);
+
+            Assert.IsTrue(expected == actual);
+        }
+    }
+}

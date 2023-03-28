0a790969441aa39383db1f11777963ed516a24fc
bookWorm <golosov2215>
2023-03-29 01:38:55 +0300

    ordered list

---
+++ Solution/Solution/Structures/OrderedList/OrderedList.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures
+{
+    public class Node<T>
+    {
+        public T value;
+        public Node<T> next, prev;
+
+        public Node(T _value)
+        {
+            value = _value;
+            next = null;
+            prev = null;
+        }
+    }
+
+    public class OrderedList<T>
+    {
+        public Node<T> head, tail;
+        private bool _ascending;
+        private Node<T> _dummy;
+
+        public OrderedList(bool asc)
+        {
+            _ascending = asc;
+            _dummy = new Node<T>(default);
+            _dummy.next = _dummy;
+            _dummy.prev = _dummy;
+            head = null;
+            tail = null;
+        }
+
+        public OrderedList(bool asc, params T[] values) : this(asc)
+        {
+            for (int i = 0; i < values.Length; ++i)
+            {
+                Add(values[i]);
+            }
+        }
+
+        public IEnumerable<T> GetEnumerable()
+        {
+            var node = _dummy.next;
+            while (node != _dummy)
+            {
+                yield return node.value;
+                node = node.next;
+            }
+        }
+
+        public int Compare(T v1, T v2)
+        {
+            if(typeof(T) == typeof(String))
+            {
+                return String.CompareOrdinal
+                (Convert.ToString(v1).Trim(), 
+                        Convert.ToString(v2).Trim());
+            }
+
+            if(typeof(T) == typeof(int))
+            {
+                var num1 = Convert.ToInt32(v1);
+                var num2 = Convert.ToInt32(v2);
+
+                return num1 == num2 ? 0 : (num1 < num2 ? -1 : 1);
+            }
+
+            throw new ArgumentException("Not type handler");
+        }
+
+        public void Add(T value)
+        {
+            var nodeAfter = _dummy.next;
+            while (nodeAfter != _dummy)
+            {
+                var compareResult = Compare(nodeAfter.value, value);
+
+                if (compareResult == 0)
+                {
+                    nodeAfter = nodeAfter.next;
+                    break;
+                }
+
+                if (_ascending && compareResult > 0)
+                {
+                    break;
+                }
+
+                if (!_ascending && compareResult < 0)
+                {
+                    break;
+                }
+
+                nodeAfter = nodeAfter.next;
+            }
+
+            nodeAfter = nodeAfter.prev;
+            var insertedNode = new Node<T>(value);
+            var tempNext = nodeAfter.next;
+            
+            nodeAfter.next = insertedNode;
+            insertedNode.next = tempNext;
+            insertedNode.prev = nodeAfter;
+            tempNext.prev = insertedNode;
+
+            head = _dummy.next;
+            tail = _dummy.prev;
+        }
+
+        public Node<T> Find(T val)
+        {
+            var node = _dummy.next;
+            while (node != _dummy)
+            {
+                if (Compare(node.value, val) == 0)
+                {
+                    return node;
+                }
+                
+                node = node.next;
+            }
+
+            return null;
+        }
+
+        public void Delete(T val)
+        {
+            var node = _dummy.next;
+            while (node != _dummy)
+            {
+                if (Compare(val, node.value) == 0)
+                {
+                    node.prev.next = node.next;
+                    node.next.prev = node.prev;
+                    return;
+                }
+                
+                node = node.next;
+            }
+
+            head = _dummy.next;
+            tail = _dummy.prev;
+        }
+
+        public void Clear(bool asc)
+        {
+            _ascending = asc;
+            _dummy.next = _dummy;
+            _dummy.prev = _dummy;
+            head = null;
+            tail = null;
+        }
+
+        public int Count()
+        {
+            int count = 0;
+            var current = _dummy.next;
+            while (current != _dummy)
+            {
+                ++count;
+                current = current.next;
+            }
+
+            return count;
+        }
+
+        public List<Node<T>> GetAll()
+        {
+            var result = new List<Node<T>>();
+            var node = _dummy.next;
+            
+            while(node != _dummy)
+            {
+                result.Add(node);
+                node = node.next;
+            }
+            return result;
+        }
+    }
+}
---
+++ Solution/Tests/Structures/OrderedList/OrderedListTests.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AlgorithmsDataStructures;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests.OrderedList
+{
+    [TestClass]
+    public class OrderedListTests
+    {
+        [TestMethod]
+        public void Constructor_CreatingListWithValuesCollection_CorrectOrder()
+        {
+            var values = new List<int> {5, 3, 7};
+            var actualList = new OrderedList<int>(true, values.ToArray());
+            values.Sort((first, second) =>
+            {
+                if (first == second)
+                    return 0;
+                if (first < second)
+                    return -1;
+
+                return 1;
+            });
+            
+            var expectedList = new OrderedList<int>(true, values.ToArray());
+
+            Assert.IsTrue(expectedList.GetEnumerable().SequenceEqual(actualList.GetEnumerable()));
+        }
+        
+        [TestMethod]
+        public void Add_AddValueToEmptyList_CorrectHeadAndTailAndList()
+        {
+            var orderedList = new OrderedList<int>(true);
+            var value = 5;
+            var expected = new OrderedList<int>(true, value);
+            orderedList.Add(value);
+
+            Assert.AreEqual(value, orderedList.head.value);
+            Assert.AreEqual(value , orderedList.tail.value);
+            Assert.IsTrue(orderedList.GetEnumerable().SequenceEqual(expected.GetEnumerable()));
+        }
+
+        [TestMethod]
+        public void Add_AddValueToListWithSeveralElements_CorrectOrder()
+        {
+            var actualList = new OrderedList<int>(true, 5, 3, 7);
+            var value = 9;
+            var expectedList = new OrderedList<int>(true, 3, 5, 7, 9);
+            actualList.Add(value);
+
+            Assert.IsTrue(expectedList.GetEnumerable().SequenceEqual(actualList.GetEnumerable()));
+        }
+
+        [TestMethod]
+        public void Add_AddValueToNotAscendingListWithSeveralElements_CorrectOrder()
+        {
+            var actualList = new OrderedList<int>(false, 5, 3, 7);
+            var value = 9;
+            var expectedList = new OrderedList<int>(false, 9, 7, 5, 3);
+            actualList.Add(value);
+
+            Assert.IsTrue(expectedList.GetEnumerable().SequenceEqual(actualList.GetEnumerable()));
+        }
+
+        [TestMethod]
+        public void Remove_RemoveSomeValue_ListWithCorrectOrder()
+        {
+            var actualList = new OrderedList<int>(false, 5, 3, 7, 9);
+            var value = 9;
+            var expectedList = new OrderedList<int>(false, 7, 5, 3);
+            actualList.Delete(value);
+
+            Assert.IsTrue(expectedList.GetEnumerable().SequenceEqual(actualList.GetEnumerable()));
+        }
+
+        [TestMethod]
+        public void Remove_ListWithoutNeededElement_SameOnStartList()
+        {
+            var actualList = new OrderedList<int>(false, 5, 3, 7);
+            var value = 9;
+            var expectedList = new OrderedList<int>(false, 7, 5, 3);
+            actualList.Delete(value);
+
+            Assert.IsTrue(expectedList.GetEnumerable().SequenceEqual(actualList.GetEnumerable()));
+        }
+
+        [TestMethod]
+        public void Remove_EmptyList_WithoutErrors()
+        {
+            var actualList = new OrderedList<int>(false);
+            actualList.Delete(9);
+        }
+
+        [TestMethod]
+        public void Find_ListWithoutNeededElement_Null()
+        {
+            var actualList = new OrderedList<int>(false, 5, 3, 7);
+            var value = 9;
+            var actual = actualList.Find(9);
+
+            Assert.AreEqual(null, actual);
+        }
+
+        [TestMethod]
+        public void Find_ListWithNeededElement_CorrectValue()
+        {
+            var actualList = new OrderedList<int>(true, 3, 7, 8, 9, 11, 25, 101);
+            var value = 99;
+            actualList.Add(value);
+            var actual = actualList.Find(99).value;
+
+            Assert.AreEqual(value, actual);
+        }
+
+        [TestMethod]
+        public void Compare_TwoStrings_CorrectValue()
+        {
+            var list = new OrderedList<string>(true);
+            Assert.AreEqual(0, list.Compare("  hello", "hello  "));
+            Assert.AreEqual(-1, list.Compare("123", "124"));
+            Assert.AreEqual(-1, list.Compare("123", "124"));
+            Assert.AreEqual(1, list.Compare("15789", "  1578"));
+        }
+
+        [TestMethod]
+        public void GetAll_SomeList_CorrectValues()
+        {
+            var values = new List<int> {3, 7, 8, 9, 11, 25, 101};
+            var list = new OrderedList<int>(false, values.ToArray());
+
+            values.Reverse();
+            Assert.IsTrue(list.GetAll().Select(node => node.value).SequenceEqual(values));
+        }
+    }
+}

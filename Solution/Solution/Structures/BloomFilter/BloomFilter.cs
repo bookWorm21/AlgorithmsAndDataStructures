84eca50a324ee66d0e92412b136850ec9ab9aec7
bookWorm <golosov2215>
2023-04-02 19:16:20 +0300

    bloom filter

---
+++ Solution/Solution/Structures/BloomFilter/BloomFilter.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections.Generic;
+using System;
+using System.IO;
+
+namespace AlgorithmsDataStructures
+{
+    public class BloomFilter
+    {
+        public int filter_len;
+
+        private int[] _bits;
+        private readonly int sectionSize;
+        private static int SingleMask; 
+
+        public BloomFilter(int f_len)
+        {
+            filter_len = f_len;
+
+            sectionSize = sizeof(int) * 8;
+            int sectionCount = (int) Math.Ceiling((float) f_len / sectionSize);
+            _bits = new int[sectionCount];
+            
+            for (int i = 0; i < sectionCount; ++i)
+            {
+                _bits[i] = 0;
+            }
+
+            for (int i = 0; i < sectionSize; ++i)
+            {
+                SingleMask |= 1 << i;
+            }
+        }
+        
+        public int Hash1(string str1)
+        {
+            int value = 17;
+            int code = 1;
+            for (int i = 0; i < str1.Length; i++)
+            {
+                code *= value;
+                code += str1[i];
+                code %= filter_len;
+            }
+
+            return code;
+        }
+
+        public int Hash2(string str1)
+        {
+            int value = 223;
+            int code = 1;
+            for (int i = 0; i < str1.Length; i++)
+            {
+                code *= value;
+                code += str1[i];
+                code %= filter_len;
+            }
+
+            return code;
+        }
+
+        public void Add(string str1)
+        {
+            int hash1 = Hash1(str1);
+            WriteBit(hash1, 1);
+            int hash2 = Hash2(str1);
+            WriteBit(hash2, 1);
+        }
+
+        public bool IsValue(string str1)
+        {
+            int hash1 = Hash1(str1);
+            int hash2 = Hash2(str1);
+            return ReadBit(hash1) > 0 && ReadBit(hash2) > 0;
+        }
+
+        public void WriteBit(int index, int value)
+        {
+            int sectionIndex = index / sectionSize;
+            int section = _bits[sectionIndex];
+            int bitNumber = index % sectionSize;
+
+            int mask = SingleMask ^ (1 << bitNumber);
+            section = (section & mask) | (value << bitNumber);
+            _bits[sectionIndex] = section;
+        }
+        
+        public int ReadBit(int index)
+        {
+            int section = _bits[index / sectionSize];
+            int bitNumber = index % sectionSize;
+            return (section & (1 << bitNumber)) > 0 ? 1 : 0;
+        }
+    }
+}
---
+++ Solution/Tests/Structures/BloomFilter/BloomFilterTests.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests.BloomFilter
+{
+    [TestClass]
+    public class BloomFilterTests
+    {
+        [TestMethod]
+        public void ReadWriteTest()
+        {
+            AlgorithmsDataStructures.BloomFilter filter = new AlgorithmsDataStructures.BloomFilter(32);
+            var actual = filter.ReadBit(0);
+            var expected = 0;
+
+            Assert.AreEqual(expected, actual);
+
+            filter.WriteBit(0, 1);
+            actual = filter.ReadBit(0);
+            expected = 1;
+            
+            Assert.AreEqual(expected, actual);
+
+            filter.WriteBit(0, 0);
+            actual = filter.ReadBit(0);
+            expected = 0;
+
+            Assert.AreEqual(expected, actual);
+
+            filter.WriteBit(1, 1);
+            filter.WriteBit(1, 1);
+
+            actual = filter.ReadBit(1);
+            expected = 1;
+
+            Assert.AreEqual(expected, actual);
+
+            filter.WriteBit(3, 1);
+            filter.WriteBit(4, 1);
+            filter.WriteBit(30, 1);
+
+            Assert.AreEqual(1, filter.ReadBit(1));
+            Assert.AreEqual(1, filter.ReadBit(4));
+            Assert.AreEqual(1, filter.ReadBit(30));
+            Assert.AreEqual(0, filter.ReadBit(31));
+        }
+
+        [TestMethod]
+        public void Add_SomeString_PresenceOnFilter()
+        {
+            var filter = new AlgorithmsDataStructures.BloomFilter(32);
+            var strings = new List<string>()
+            {
+                "0123456789",
+                "1234567890",
+                "2345678901",
+                "3456789012",
+                "4567890123",
+                "5678901234",
+                "6789012345",
+                "7890123456",
+                "8901234567",
+                "9012345678",
+            };
+
+            foreach (var str in strings)
+            {
+                filter.Add(str);
+            }
+
+            foreach (var str in strings)
+            {
+                Assert.AreEqual(true, filter.IsValue(str));
+            }
+        }
+    }
+}

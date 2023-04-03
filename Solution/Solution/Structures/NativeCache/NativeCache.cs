21f1d3382a45c4ebb797ffe3815f45f19c6a8202
bookWorm <golosov2215>
2023-04-04 00:54:06 +0300

    pinky realization native cache

---
+++ Solution/Solution/Structures/NativeCache/NativeCache.cs
@@ -0,0 +1,161 @@
+﻿using System;
+
+namespace AlgorithmsDataStructures
+{
+    public class NativeCache<T>
+    {
+        public int size;
+        public string [] slots;
+        public T [] values;
+        public int[] hints;
+        public const int Step = 1;
+
+        public NativeCache(int sz)
+        {
+            size = sz;
+            slots = new string[size];
+            values = new T[size];
+            hints = new int[size];
+
+            for (int i = 0; i < size; ++i)
+            {
+                slots[i] = null;
+                values[i] = default;
+                hints[i] = default;
+            }
+        }
+
+        public int HashFun(string key)
+        {
+            int hash = 0;
+            foreach (var sym in key)
+            {
+                hash += sym.GetHashCode();
+            }
+            return hash % size;
+        }
+
+        public bool IsKey(string key)
+        {
+            if (key == null)
+            {
+                return false;
+            }
+            
+            int hashIndex = HashFun(key);
+            int stepScale = 0;
+            while (stepScale < size)
+            {
+                if (slots[hashIndex] == null)
+                {
+                    return false;
+                }
+                
+                if (slots[hashIndex] == key)
+                {
+                    return true;
+                }
+                    
+                hashIndex += Step;
+                hashIndex %= size;
+                stepScale += Step;
+            }
+            
+            return false;
+        }
+
+        public void Put(string key, T value)
+        {
+            if (key == null)
+            {
+                return;
+            }
+            
+            int hashIndex = HashFun(key);
+            int stepScale = 0;
+            int minHint = Int32.MaxValue;
+            int minIndex = -1;
+
+            while (stepScale < size)
+            {
+                if (slots[hashIndex] == null)
+                {
+                    slots[hashIndex] = key;
+                    values[hashIndex] = value;
+                    hints[hashIndex] = 1;
+                    return;
+                }
+                
+                if (slots[hashIndex] == key)
+                {
+                    values[hashIndex] = value;
+                    hints[hashIndex] = 1;
+                    return;
+                }
+
+                if (hints[hashIndex] < minHint)
+                {
+                    minHint = hints[hashIndex];
+                    minIndex = hashIndex;
+                }
+                
+                hashIndex += Step;
+                hashIndex %= size;
+                stepScale += Step;
+            }
+            
+            slots[minIndex] = key;
+            values[minIndex] = value;
+            hints[minIndex] = 1;
+        }
+
+        public T Get(string key)
+        {
+            int hashIndex = HashFun(key);
+            int stepScale = 0;
+            while (stepScale < size)
+            {
+                if (slots[hashIndex] == null)
+                {
+                    return default;
+                }
+                
+                if (slots[hashIndex] == key)
+                {
+                    hints[hashIndex] += 1;
+                    return values[hashIndex];
+                }
+                
+                hashIndex += Step;
+                hashIndex %= size;
+                stepScale += Step;
+            }
+            
+            return default;
+        }
+
+        public int GetKeyIndex(string key)
+        {
+            int hashIndex = HashFun(key);
+            int stepScale = 0;
+            while (stepScale < size)
+            {
+                if (slots[hashIndex] == null)
+                {
+                    return -1;
+                }
+                
+                if (slots[hashIndex] == key)
+                {
+                    return hashIndex;
+                }
+                
+                hashIndex += Step;
+                hashIndex %= size;
+                stepScale += Step;
+            }
+            
+            return -1;
+        }
+    }
+}
---
+++ Solution/Tests/NativeCache/NativeCacheTests.cs
@@ -0,0 +1,53 @@
+﻿using System.Runtime.CompilerServices;
+using AlgorithmsDataStructures;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests.NativeCache
+{
+    [TestClass]
+    public class NativeCacheTests
+    {
+        [TestMethod]
+        public void TestHintIncreasing()
+        {
+            var cache = new NativeCache<int>(5);
+            var first = "first";
+            var second = "second";
+            var third = "third";
+            var four = "four";
+            var five = "five";
+            var six = "six";
+
+            cache.Put(first, 1);
+            cache.Put(second, 2);
+            cache.Put(third, 3);
+            cache.Put(four, 4);
+            cache.Put(five, 5);
+            
+            Assert.AreEqual(1, cache.hints[0]);
+            Assert.AreEqual(1, cache.hints[1]);
+            Assert.AreEqual(1, cache.hints[2]);
+            Assert.AreEqual(1, cache.hints[3]);
+            Assert.AreEqual(1, cache.hints[4]);
+            
+            int thirdIndex = cache.GetKeyIndex(third);
+            var thirdValue = cache.Get(third);
+
+            Assert.AreEqual(3, thirdValue);
+            Assert.AreEqual(2, cache.hints[thirdIndex]);
+            
+            cache.Get(second);
+            cache.Get(four);
+            cache.Get(five);
+
+            var firstIndex = cache.GetKeyIndex(first);
+            cache.Put(six, 6);
+
+            var sixIndex = cache.GetKeyIndex(six);
+
+            Assert.AreEqual(firstIndex, sixIndex);
+            Assert.AreEqual(1, cache.hints[firstIndex]);
+            Assert.AreEqual(6, cache.Get(six));
+        }
+    }
+}
